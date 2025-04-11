11b61cd344549dceab0eb26eb8ca631882058c28
Andrey Smirnoff <j75yhciamoc@gmail.com>
2025-04-11 15:39:49 +0200

    test

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -2,6 +2,6 @@ import { Request, Response } from 'express'
 
 export async function admin(req: Request, res: Response): Promise<Response | any> {
     const { username } = req.params
-    
+    return username
 
 }
