ba5512dada8f5953e50d0f770d7182356ce2a0a9
Andrey Smirnoff <j75yhciamoc@gmail.com>
2025-04-15 16:58:52 +0200

    finished products logic

---
+++ src/types/Product.ts
@@ -0,0 +1,15 @@
+export type TypeProduct = {
+    id: bigint
+    brand: string
+    title: string
+    model: string
+    type: string
+    description: string
+    year: number
+    ps: number
+    pricePerDay: number
+    color: string
+    photoUrls: string[]
+    rentStart: Date
+    rentEnd: Date
+}
