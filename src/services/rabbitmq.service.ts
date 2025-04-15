299fa111d1daceb2b5a7778be34782f54ce75c07
Andrey Smirnoff <j75yhciamoc@gmail.com>
2025-04-15 20:18:20 +0200

    changed awwor functions to usual

--- src/services/rabbitmq.service.ts
+++ src/services/rabbitmq.service.ts
@@ -2,15 +2,14 @@ import amqplib from 'amqplib'
 
 let channel: amqplib.Channel | null = null
 
-
-export const initRabbitMQ = async (): Promise<void> => {
+export async function initRabbitMQ(): Promise<void>{
     const connection = await amqplib.connect(process.env.RABBITMQ_URL as string)
     channel = await connection.createChannel()
     await channel.assertQueue("push_notifications", { durable: true })
 }
 
-export const getRabbitChannel = (): amqplib.Channel => {
-    if (!channel) {
+export function getRabbitChannel(): amqplib.Channel{
+    if(!channel){
         throw new Error("RabbitMQ channel is not initialized")
     }
     return channel
