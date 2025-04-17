d46576aa23076780f934f6b1ba47852538f0118b
Andrey Smirnoff <j75yhciamoc@gmail.com>
2025-04-17 13:31:26 +0200

    configured credentials

--- src/services/s3.bucket.service.ts
+++ src/services/s3.bucket.service.ts
@@ -1,7 +1,6 @@
 import { S3Client } from '@aws-sdk/client-s3'
 
 export const s3 = new S3Client({
-    region: process.env.REGION,
     credentials: {
         accessKeyId: process.env.S3_ACCESS_KEY as string,
         secretAccessKey: process.env.S3_SECRET_KEY as string
