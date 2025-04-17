0eed6fe2d39885a10ad845e1299d8ebff7d6379d
Andrey Smirnoff <j75yhciamoc@gmail.com>
2025-04-18 00:58:55 +0200

    added routes

--- app.ts
+++ app.ts
@@ -3,10 +3,7 @@ import authRoutes from '#src/routes/auth.routes.js'
 import verificationRoutes from '#src/routes/verification.routes.js'
 import paymentRoutes from '#src/routes/payment.routes.js'
 import userRoutes from '#src/routes/user.routes.js'
-import * as swaggerUi from 'swagger-ui-express'
-import openapi from './openapi.json' with { type: "json" }
 
-router.use("/docs", swaggerUi.serve, swaggerUi.setup(openapi))
 router.use('/auth', authRoutes)
 router.use("/verification", verificationRoutes)
 router.use("/payments", paymentRoutes)
